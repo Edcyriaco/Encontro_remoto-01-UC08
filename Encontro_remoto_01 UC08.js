6f6cfb7556136ac6d7fbdc8675362f6033ce7bad
Ednaldo Cyriaco <97308257+Edcyriaco@users.noreply.github.com>
2022-09-10 00:47:36 -0300

    Atividade do encontro remoto 01-UC08 turma-06
    Atividade ministrada pelo professor Caique Zaneti

---
+++ Encontro_remoto_01 UC08.js
@@ -0,0 +1,74 @@
+var listaDePecas = ["Amortecedor", "motor", "Filtro de ar"]
+
+let peso = 50
+if(peso < 100){
+    console.log ("A peça deve pesar no minimo 100g")
+}else{
+    console.log ("A peça possui peso adequado")
+}
+
+let peso = 100
+if(peso < 100){
+    console.log ("A peça deve pesar no minimo 100g")
+}else{
+    console.log("A peça possui peso adequado")
+}
+
+
+var listaDePecas = ["Amortecedor", "motor", "Filtro de ar"]
+if(listaDePecas .length <10){
+    console.log ("Ainda tem espaço")
+}else{
+    console.log ("Não tem mais espaço")
+}
+
+
+var listaDePecas = ["Amortecedor", "motor", "Filtro de ar","Correia dentada", "Biela", "Virabrequim","Catalizador", "Homocinetica", "Pistão"]
+if(listaDePecas .length <10){
+    console.log ("Ainda tem espaço")
+}else{
+    console.log ("Não tem mais espaço")
+}
+
+
+var listaDePecas = ["Amortecedor", "motor", "Filtro de ar", "Correia dentada", "Biela", "Virabrequim","Catalizador", "Homocinetica", "Pistão", "Cárter"]
+if(listaDePecas .length <10){
+    console.log ("Ainda tem espaço")
+}else{
+    console.log ("Não tem mais espaço")
+}
+
+
+let nomePeca = "disco de Freio"
+if(nomePeca.length > 3 ) {
+ console.log("Nome da peça esta adequado")
+}else if (nomePeca.length == 0) {
+    console.log("Nome da peça não pode ser vazio")
+}else {
+    console.log("Nome da peça precisa ter no minimo 3 caracteres")
+
+}
+
+
+
+let nomePeca = "Di"
+if(nomePeca.length > 3 ) {
+ console.log("Nome da peça esta adequado")
+}else if (nomePeca.length == 0) {
+    console.log("Nome da peça não pode ser vazio")
+}else {
+    console.log("Nome da peça precisa ter no minimo 3 caracteres")
+
+}
+
+
+
+let nomePeca = ""
+if(nomePeca.length > 3 ) {
+ console.log("Nome da peça esta adequado")
+}else if (nomePeca.length == 0) {
+    console.log("Nome da peça não pode ser vazio")
+}else {
+    console.log("Nome da peça precisa ter no minimo 3 caracteres")
+
+}
